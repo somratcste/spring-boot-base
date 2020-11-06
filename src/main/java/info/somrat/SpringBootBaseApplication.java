45a0396fec373b1b21ce555ed675e452d1159093
nazmul <somratcste@gmail.com>
2020-11-06 20:56:26 +0100

    chore: support war packaging

--- src/main/java/info/somrat/SpringBootBaseApplication.java
+++ src/main/java/info/somrat/SpringBootBaseApplication.java
@@ -2,12 +2,19 @@ package info.somrat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootBaseApplication {
+public class SpringBootBaseApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootBaseApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootBaseApplication.class);
     }
 
 }
