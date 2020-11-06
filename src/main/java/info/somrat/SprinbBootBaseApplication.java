34ddee239a5120bf740f0b5903329426dca2c266
nazmul <somratcste@gmail.com>
2020-11-06 20:33:45 +0100

    initialize

---
+++ src/main/java/info/somrat/SprinbBootBaseApplication.java
@@ -0,0 +1,13 @@
+package info.somrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbBootBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinbBootBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/somrat/SprinbBootBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package info.somrat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbBootBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
