387ad2037912b9c9a0a6404cd0857aa5430935c4
somratcste <somratcste@gmail.com>
2021-01-02 06:10:30 +0100

    feat: create a custom email validator

--- src/main/java/info/somrat/rest/request/SignUpRequest.java
+++ src/main/java/info/somrat/rest/request/SignUpRequest.java
@@ -1,17 +1,33 @@
 package info.somrat.rest.request;
 
+import info.somrat.rest.validators.UniqueEmail;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequest {
 
     @NotBlank
+    @Size(min = 3, max = 20)
     private String username;
 
     @NotBlank
+    @Size(max = 50)
+    @Email
+    @UniqueEmail
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
     private String password;
 }
---
+++ src/main/java/info/somrat/rest/validators/UniqueEmailValidator.java
@@ -0,0 +1,20 @@
+package info.somrat.rest.validators;
+
+import info.somrat.rest.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Component
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return userRepository.findByEmail(email) == null;
+    }
+}
