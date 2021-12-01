bf6a38ff5eaec007a65f7e8c3ecb00b7bf598b50
somratcste <somratcste@gmail.com>
2021-12-01 16:54:43 +0100

    task: integration test for user controlelr

--- src/main/java/info/somrat/rest/controller/UserController.java
+++ src/main/java/info/somrat/rest/controller/UserController.java
@@ -1,12 +1,15 @@
 package info.somrat.rest.controller;
 
 import info.somrat.rest.models.Post;
+import info.somrat.rest.response.PageResponse;
 import info.somrat.rest.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,11 +21,12 @@ public class UserController {
 
     @PreAuthorize("hasRole('USER')")
     @GetMapping("/{id}/posts")
-    public ResponseEntity<?> getAllPostByUser(@PathVariable("id") long userId,
+    public ResponseEntity<PageResponse> getAllPostByUser(@PathVariable("id") long userId,
                                                        @RequestParam(defaultValue = "0") int pageNo,
                                                        @RequestParam(defaultValue = "10") int pageSize
                                          ) {
         Page<Post> postPage= postService.getAllPostsByUser(pageNo, pageSize, userId);
-        return ResponseEntity.ok(postPage.getContent());
+        return ResponseEntity.ok(new PageResponse(true, "", postPage.getTotalElements(), postPage.getTotalPages(),
+                postPage.getNumber(), Arrays.asList(postPage.getContent().toArray())));
     }
 }
---
+++ src/test/java/info/somrat/controller/UserControllerIT.java
@@ -0,0 +1,55 @@
+package info.somrat.controller;
+
+import info.somrat.rest.request.LoginRequest;
+import info.somrat.rest.response.JwtResponse;
+import info.somrat.rest.response.PageResponse;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserControllerIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    private final String BASE_URL = "/api/users";
+    HttpHeaders headers;
+
+    @BeforeAll
+    public void getUserRequest() {
+        LoginRequest user = new LoginRequest("hossain", "123456");
+        HttpEntity<LoginRequest> loginRequest = new HttpEntity<>(user);
+        ResponseEntity<JwtResponse> result = restTemplate.postForEntity("/api/auth/signin", loginRequest, JwtResponse.class);
+        headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + result.getBody().getToken());
+        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+    }
+
+    /**
+     * Get user id from dummy data
+     */
+    @Test
+    public void getAllPostByUserSuccess() {
+        Map<String, String> uriVariables = new HashMap<>();
+        // assign user id
+        uriVariables.put("id", "1");
+        final String url = "http://localhost:" + port + BASE_URL + "/{id}/posts";
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+        ResponseEntity<PageResponse> result = restTemplate.exchange( url, HttpMethod.GET, request, PageResponse.class, uriVariables);
+        assertEquals(0, result.getBody().getTotalItems());
+    }
+
+}
